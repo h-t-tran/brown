a20ad7e44950f072a6205ac241a56eec3b3c8237
Hoanh Tran <hoanh.tran@gdit.com>
2016-05-23 11:00:44 -0700

    add more extjs

--- typescript/code/calculator/extjs-demo/app.js
+++ typescript/code/calculator/extjs-demo/app.js
@@ -48,15 +48,62 @@ Ext.application({
     // --------------------------------------
     testSomeUI: function () {
         Ext.create('Ext.container.Viewport', {
-            name: "viewPort2",
-            layout: 'fit',
-            items: [
-                {
-                    title: 'My Viewport',
-                    html: 'Hello World!'
-                }
-            ]
+            layout: 'border',
+            items: [{
+                    region: 'north',
+                    html: '<h1 class="x-panel-header">Page Title</h1>',
+                    autoHeight: true,
+                    border: false,
+                    margins: '0 0 5 0'
+                }, {
+                    region: 'west',
+                    collapsible: true,
+                    title: 'Navigation',
+                    width: 150
+                }, {
+                    region: 'south',
+                    title: 'South Panel',
+                    collapsible: true,
+                    html: 'Information goes here',
+                    split: true,
+                    height: 100,
+                    minHeight: 100
+                }, {
+                    region: 'east',
+                    title: 'East Panel',
+                    collapsible: true,
+                    split: true,
+                    width: 150
+                }, {
+                    region: 'center',
+                    xtype: 'tabpanel',
+                    activeTab: 0,
+                    items: [{
+                            title: 'Default Tab',
+                            html: 'The first tab\'s content. Others may be added dynamically',
+                        }
+                    ]
+                }]
         });
+        //Ext.create('Ext.Container', {
+        //    renderTo: Ext.getBody(),
+        //    items   : [
+        //        {
+        //            xtype: 'button',
+        //            text : 'My Button'
+        //        }
+        //    ]
+        //});
+        //Ext.create('Ext.container.Viewport', {
+        //    name : "viewPort2",
+        //    layout: 'fit',
+        //    items: [
+        //        {
+        //            title: 'My Viewport',
+        //            html : 'Hello World!'
+        //        }
+        //    ]
+        //});
     },
     // --------------------------------------
     // Methods
